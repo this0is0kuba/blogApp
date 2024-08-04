3dc364026d29971af37cbb000c54a2261f1812a2
this0is0kuba <jakub.rakus06@gmail.com>
2024-08-04 20:12:55 +0200

    update main.py

--- backendBlogApp/app/main.py
+++ backendBlogApp/app/main.py
@@ -1,23 +1,14 @@
 from fastapi import FastAPI
-from sqlmodel import create_engine, SQLModel
+from database import create_db_and_tables, drop_database
 
 # lines needed to initialize all models properly (It's necessary to import them to the main.py file)
 from models import User, Blog, Role, Comment, Tag, Category  # noqa
 
-sqlite_file_name = "database.db"
-sqlite_url = f"sqlite:///{sqlite_file_name}"
-
-connect_args = {"check_same_thread": False}
-engine = create_engine(sqlite_url, echo=True, connect_args=connect_args)
-
 app = FastAPI()
-
-
-def create_db_and_tables():
-    SQLModel.metadata.create_all(engine)
 
 
 @app.on_event("startup")
 def on_startup():
-    create_db_and_tables()
 
+    drop_database()
+    create_db_and_tables()
